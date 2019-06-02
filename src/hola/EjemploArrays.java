b3e6ca7bd2fa7fd934c745fa63d7b235216bdab2
J <joseba.3.1.3@hotmail.com>
2019-06-02 22:10:46 +0200

    Ejercicio finalizado

--- src/hola/EjemploArrays.java
+++ src/hola/EjemploArrays.java
@@ -29,7 +29,7 @@ public class EjemploArrays {
 		//
 		System.out.println("--------------------------------------------");
 		elementoBuscar = ObtencionTeclado.recogerEnteroTeclado("Introduzca el valor a buscar en el array ordenado: ");
-		posicionArray = buscarElementoArrayBusquedaLineal(arrayOrdenadoEnterosAleatorios, elementoBuscar);
+		posicionArray = buscarElementoArrayOrdenadoBusquedaLineal(arrayOrdenadoEnterosAleatorios, elementoBuscar);
 		System.out.println("La posicion del elemento en el array es: " + posicionArray);
 		//
 		System.out.println("--------------------------------------------");
@@ -37,6 +37,8 @@ public class EjemploArrays {
 				.recogerEnteroTeclado("Introduzca el valor a buscar (búsqueda binaria) en el array ordenado: ");
 		posicionArray = buscarElementoArrayOrdenadoBusquedaBinaria(arrayOrdenadoEnterosAleatorios, elementoBuscar);
 		System.out.println("La posicion del elemento en el array es: " + posicionArray);
+		
+		
 	}
 
 	public static int[] generarArrayEnterosAleatorios(int longitud, int valorMaximo) {
@@ -60,10 +62,19 @@ public class EjemploArrays {
 	public static int buscarElementoArrayBusquedaLineal(int[] arrayEnterosSinOrdenar, int elementoBuscar) {
 
 		int posicion = -1;
-		
-		//Array.indexOf(elementoBuscar);
-	
-	
+		// indexof()
+
+		boolean encontrado = false;
+		int i = 0;
+		while ((i < arrayEnterosSinOrdenar.length) && (!encontrado)) {
+			if (arrayEnterosSinOrdenar[i] == elementoBuscar) {
+				encontrado = true;
+				posicion = i;
+			} else {
+				i = i + 1;
+			}
+		}
+
 		return posicion;
 	}
 
@@ -71,20 +82,34 @@ public class EjemploArrays {
 		/*
 		 * Metodo de arrays,su funcionamiento es empezar a ordenar de menor a mayor,en
 		 * caso de que el array seria string basicamente lo ordenaria comparando el
-		 * abecerario en caso de que necesitariamos revertir el orden habria que llamar
-		 * al metodo Collections.reverseOrder() dentro del array.sort,tambien hay otro
-		 * metodo implementado en java 8 llamado Arrays.ParallelSort() que es mas rapido
-		 * pero cuando los elementos de un array son pocos no hay diferencia,la
-		 * diferencia entre los dos metodos es que sort solo funciona con un hilo y
-		 * ParallelSort con multiples lo que hace que sea mas veloz
+		 * abecerario.
+		 * 
+		 * En caso de que necesitariamos revertir el orden habria que llamar al metodo
+		 * Collections.reverseOrder() dentro del array.sort,tambien hay otro metodo
+		 * implementado en java 8 llamado Arrays.ParallelSort() que es mas rapido pero
+		 * cuando los elementos de un array son pocos no hay diferencia,la diferencia
+		 * entre los dos metodos es que sort solo funciona con un hilo y ParallelSort
+		 * con multiples lo que hace que sea mas veloz
 		 */
 		Arrays.sort(arrayEnteros);
 
 		return arrayEnteros;
 	}
 
-	public static int buscarElementoArrayOrdenadoBusquedaLineal(int[] arrayEnterosSinOrdenar, int elementoBuscar) {
+	public static int buscarElementoArrayOrdenadoBusquedaLineal(int[] arrayEnterosOrdenados, int elementoBuscar) {
 		int posicion = -1;
+		boolean encontrado = false;
+		int i = 0;
+
+		while ((i < arrayEnterosOrdenados.length) && (!encontrado)) {
+			if (arrayEnterosOrdenados[i] == elementoBuscar) {
+				encontrado = true;
+				posicion = i;
+			} else {
+				i = i + 1;
+			}
+
+		}
 		return posicion;
 	}
 
@@ -97,6 +122,14 @@ public class EjemploArrays {
 
 		return busqueda_binaria;
 	}
+	
+	
+	
+	/*************Otras formas******************/
+	
+	
+	
+	
 
 	public static int buscarElementoArrayBusquedaLinealBueno(int[] arrayEnterosSinOrdenar, int elementoBuscar) {
 		int posicion = -1;
